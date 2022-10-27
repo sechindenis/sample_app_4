8e062e0cfa64bcfccda0fdb6ad9f56b9fdba43ad
Denis Sechin <sechin@LAPTOP-L448O8CJ.localdomain>
2022-10-27 23:41:52 +0500

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -15,4 +16,26 @@ class User < ApplicationRecord
             BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # Возвращает случайный токен.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Запоминает пользователя в базе данных для использования в постоянных сеансах.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Возвращает true, если указанный токен соответствует дайджесту.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Забывает пользователя
+  def forget
+    update_attribute(:remember_digest, nil)
+  end 
 end
