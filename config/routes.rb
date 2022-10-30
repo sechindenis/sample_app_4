8b2cb411241f4b2fcdd25cff3eaa96ca018f4d2c
Denis Sechin <sechin@LAPTOP-L448O8CJ.localdomain>
2022-10-30 22:45:51 +0500

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home'
   get '/help',      to: 'static_pages#help'
   get '/about',     to: 'static_pages#about'
@@ -10,5 +12,6 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   default_url_options :host => "example.com"
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
   
