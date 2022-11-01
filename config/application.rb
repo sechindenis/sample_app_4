0a2f01b54a37a019311c191d0a2472eaf078d1c0
Denis Sechin <sechin@LAPTOP-L448O8CJ.localdomain>
2022-11-02 01:00:05 +0500

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module SampleApp4
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Включать токен аутентификации в удаленные формы.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
